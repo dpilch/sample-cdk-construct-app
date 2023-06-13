bf899bb25f22ba4ace51f3e839615a64b89c072a
Dane Pilcher <dppilche@amazon.com>
2023-06-13 12:51:57 -0600

    ci: set my repo

--- backend/stacks/deployment-stack.ts
+++ backend/stacks/deployment-stack.ts
@@ -1,8 +1,12 @@
-import { Construct } from 'constructs';
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
-import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
-import { App, Branch, GitHubSourceCodeProvider } from '@aws-cdk/aws-amplify-alpha';
+import { Construct } from "constructs";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Role, ServicePrincipal, ManagedPolicy } from "aws-cdk-lib/aws-iam";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import {
+  App,
+  Branch,
+  GitHubSourceCodeProvider,
+} from "@aws-cdk/aws-amplify-alpha";
 
 export type DeploymentStackProps = StackProps & {
   githubAccessToken: Secret;
@@ -10,28 +14,28 @@ export type DeploymentStackProps = StackProps & {
 
 /**
  * Deploy a service-linked-role for Amplify to use to deploy CDK,
- * then 
+ * then
  */
 export class DeploymentStack extends Stack {
   constructor(scope: Construct, id: string, props: DeploymentStackProps) {
     super(scope, id, props);
 
-    const serviceRole = new Role(this, 'AmplifyServiceRole', {
-      assumedBy: new ServicePrincipal('amplify.amazonaws.com'),
+    const serviceRole = new Role(this, "AmplifyServiceRole", {
+      assumedBy: new ServicePrincipal("amplify.amazonaws.com"),
       managedPolicies: [
-        ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess-Amplify'),
+        ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess-Amplify"),
       ],
     });
 
-    const app = new App(this, 'AmplifyApp', {
+    const app = new App(this, "AmplifyApp", {
       sourceCodeProvider: new GitHubSourceCodeProvider({
-        owner: 'aherschel',
-        repository: 'sample-cdk-construct-app',
+        owner: "dpilch",
+        repository: "sample-cdk-construct-app",
         oauthToken: props.githubAccessToken.secretValue,
       }),
       role: serviceRole,
     });
 
-    new Branch(this, 'main', { app });
+    new Branch(this, "main", { app });
   }
 }
